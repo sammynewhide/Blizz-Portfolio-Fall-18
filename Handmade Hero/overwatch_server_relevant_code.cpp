153f878a886e59877460151a6614f41aaf56b7d2
BoomalaWorks <sam2000new@gmail.com>
2018-12-07 12:40:55 -0800

    Update overwatch_server_relevant_code.cpp

--- Handmade Hero/overwatch_server_relevant_code.cpp
+++ Handmade Hero/overwatch_server_relevant_code.cpp
@@ -2,8 +2,8 @@
 ### ~~~ For more context of the project please view the "read_me_first" document in 'Blizz-Portfolio-Fall-18/Handmade Hero' ~~~ ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-### Explanation of the code in here might be limited due to Finals Week studying. To see the the code in the main gameplay loop,
-### please see 'Blizz-Portfolio-Fall-18/Handmade Hero/code/handmade.cpp'. Thank you for the understanding.
+### Explanation of the code in here might be limited due to Finals Week studying. To see code regarding data structures,
+### please see 'Blizz-Portfolio-Fall-18/Handmade Hero/code/handmade.cpp' and CRTL+F search for 'AddLowEntity'. Thank you for the understanding.
 
 ### "Sparse Entity Storage" ###
 
