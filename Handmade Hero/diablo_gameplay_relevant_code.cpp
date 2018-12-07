aada7d482f9e716a4089dd2207efee73ac0aa7a8
sammynewhide <sam2000new@gmail.com>
2018-12-07 10:21:34 -0800

    Code explanation for Diablo - Gameplay position

--- Handmade Hero/diablo_gameplay_relevant_code.cpp
+++ Handmade Hero/diablo_gameplay_relevant_code.cpp
@@ -1,3 +1,6 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ~~~ For more context of the project please view the "read_me_first" document in 'Blizz-Portfolio-Fall-18/Handmade Hero' ~~~ ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+### Explanation of the code in here might be limited due to Finals Week studying. To see the the code in the main gameplay loop,
+### please see 'Blizz-Portfolio-Fall-18/Handmade Hero/code/handmade.cpp'. Thank you for the understanding.
