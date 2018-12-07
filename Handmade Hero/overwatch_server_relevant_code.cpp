954261d8d6c09fd614b1d688170c302773aeebbb
sammynewhide <sam2000new@gmail.com>
2018-12-07 10:26:27 -0800

    Explanation of code for the Overwatch - Server position

--- Handmade Hero/overwatch_server_relevant_code.cpp
+++ Handmade Hero/overwatch_server_relevant_code.cpp
@@ -2,6 +2,9 @@
 ### ~~~ For more context of the project please view the "read_me_first" document in 'Blizz-Portfolio-Fall-18/Handmade Hero' ~~~ ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
+### Explanation of the code in here might be limited due to Finals Week studying. To see the the code in the main gameplay loop,
+### please see 'Blizz-Portfolio-Fall-18/Handmade Hero/code/handmade.cpp'. Thank you for the understanding.
+
 ### "Sparse Entity Storage" ###
 
 // There was once a time in the game where every position in the world
